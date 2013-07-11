9e8475be1f63f0c61b64d129471a8c368b425eaf
mbal <matteo.bana@gmail.com>
2013-07-11 19:09:10 +0200

    comment

--- new.scm
+++ new.scm
@@ -6,11 +6,13 @@
   (helper list '()))
 
 (define-syntax create-fsm
+  ;; like Petrovsky suggests, we can use a string to disambiguate among
+  ;; patterns, since patterns are matched using (equal? x y)
   (syntax-rules ()
     ((create-fsm name (state transitions ...) ...) 
-     (create-fsm "grouped"  ;; so we can actually match the second pattern, 
-                            ;; and not this again
-                 #f        ;; don't check
+     (create-fsm "grouped"  ; so we can actually match the second pattern, 
+                 ; and not this again
+                 #f        ; don't check
                  name 
                  (state ...) ;; the list of all states in the fsm
                  ((state transitions ...) ...)))
@@ -38,48 +40,48 @@
 ;; starting state.
 (define-syntax get-first
   (syntax-rules ()
-    ((get-first state states ...) state)))
+                ((get-first state states ...) state)))
 
 (begin-for-syntax
- ;; returns all the transitions that are outgoing (i.e. refers to a state
- ;; which is not declared in the FSM)
- ;; (get-outward-transitions (a -> B)) if B is not a FSM state, returns B.
- ;; if a FSM does have one or more outgoing transitions, it's invalid
- ;; and it's rejected at compile-time (if the check is enabled).
+  ;; returns all the transitions that are outgoing (i.e. refers to a state
+  ;; which is not declared in the FSM)
+  ;; (get-outward-transitions (a -> B)) if B is not a FSM state, returns B.
+  ;; if a FSM does have one or more outgoing transitions, it's invalid
+  ;; and it's rejected at compile-time (if the check is enabled).
 
- (define (get-outward-transitions states transitions)
-   ;; this function returns false when the item is in the list, 
-   ;; returns the item if it's present.
-   (define (not-in? list what)
-     (cond ((null? list) what)
-           ((equal? (car list) what) #f)
-           (else (not-in? (cdr list) what))))
-   (filter (lambda (x) x) (map (lambda (x) (not-in? states x)) transitions))))
+  (define (get-outward-transitions states transitions)
+    ;; this function returns false when the item is in the list, 
+    ;; returns the item if it's present.
+    (define (not-in? list what)
+      (cond ((null? list) what)
+            ((equal? (car list) what) #f)
+            (else (not-in? (cdr list) what))))
+    (filter (lambda (x) x) (map (lambda (x) (not-in? states x)) transitions))))
 
 ;;; I'm quite satisfied with this macro.
 (define-syntax checked-expander
   (ir-macro-transformer
-   (lambda (data ren cmp)
-     (let* ((statename (cadr data)) ; the first element is the macro's name
-            (states (caddr data)) ; the args after that are the actual params
-            (transitions (cdddr data))
-            (offending-transitions
-             (get-outward-transitions states (map cadr transitions))))
-       (if (equal? offending-transitions '())
-           `(lambda (in)
-              (cond ((null? in) #f)
-                    ,@(map
+    (lambda (data ren cmp)
+      (let* ((statename (cadr data)) ; the first element is the macro's name
+             (states (caddr data)) ; the args after that are the actual params
+             (transitions (cdddr data))
+             (offending-transitions
+               (get-outward-transitions states (map cadr transitions))))
+        (if (equal? offending-transitions '())
+          `(lambda (in)
+             (cond ((null? in) #f)
+                   ,@(map
                        (lambda (x)
                          `((equal? (car in) ',(car x))
                            ,@(if (>= (length x) 3)
-                                 (map (lambda (y) `(,y in)) (cddr x))
-                                 '())
+                               (map (lambda (y) `(,y in)) (cddr x))
+                               '())
                            (,(cadr x) (cdr in))))
                        transitions)
-                    (else #f)))
-           (syntax-error statename
-                         "Transitions go out of the FSM"
-                         offending-transitions))))))
+                   (else #f)))
+          (syntax-error statename
+                        "Transitions go out of the FSM"
+                        offending-transitions))))))
 
 (define-syntax process-state2
   (syntax-rules (-> end)
@@ -91,7 +93,7 @@
      ;; here we are going to perform a check. We need a "common lisp" macro
      ;; because we need lower level control on the generated code.
      ;; However, we use an ir-* macro since we care about hygene.
-      (checked-expander statename states (input next callback ...) ...))
+     (checked-expander statename states (input next callback ...) ...))
     ((_ statename #f states (input -> next callback ...) ...)
      ;; this clause doesn't perform a validity check for the outgoing states
      (lambda (in)
@@ -99,7 +101,7 @@
              ((equal? (car in) 'input) 
               (callback in) ...
               (next (cdr in))) ...
-              (else #f))))))
+             (else #f))))))
 
 
 (create-fsm test
